91f0dc5344eb2897c0db85928c1c7539d05cf826
ROSALVO Ricardo <rosalvoricardo@gmail.com>
2024-01-30 09:48:36 -0600

    corect add lo

--- app/Http/Controllers/ajouterLotGagnantController.php
+++ app/Http/Controllers/ajouterLotGagnantController.php
@@ -9,201 +9,197 @@ use Illuminate\Validation\Rule;
 use Illuminate\Support\Facades\Validator;
 use App\Models\tirage_record;
 use Illuminate\Support\Carbon;
+
 class ajouterLotGagnantController extends Controller
 {
-    
-    public function index(){
-    $list=BoulGagnant::where('compagnie_id',session('loginId'))->orderBy('created_at', 'desc')->get();
-    return view('list-lo',compact('list'));
-    }
-    public function ajouterlo(Request $request){
-          $id=$request->id;
-          
-          $list=tirage_record::where('compagnie_id',session('loginId'))->get();
-          $record=BoulGagnant::where('compagnie_id',session('loginId'))->where('created_',$id)->first();
-          return view('ajoutelo',compact('list','record'));
-    }
-    
-    public function store(Request $request){
 
- 
-        $tirageId=$request->input('tirage');
-        $date=$request->input('date');
-        $unchiffre=$request->input('unchiffre');
-        $premierchiffre=$request->input('premierchiffre');
-        $secondchiffre=$request->input('secondchiffre');
-        $troisiemechiffre=$request->input('troisiemechiffre');
+    public function index()
+    {
+        $list = BoulGagnant::where('compagnie_id', session('loginId'))->orderBy('created_at', 'desc')->get();
+        return view('list-lo', compact('list'));
+    }
+    public function ajouterlo(Request $request)
+    {
+        $id = $request->id;
 
-        $erreurs =$this->validerEntrees($tirageId, $unchiffre, $premierchiffre, $secondchiffre, $troisiemechiffre);
+        $list = tirage_record::where('compagnie_id', session('loginId'))->get();
+        $record = BoulGagnant::where('compagnie_id', session('loginId'))->where('created_', $id)->first();
+        return view('ajoutelo', compact('list', 'record'));
+    }
+
+    public function store(Request $request)
+    {
+
+
+        $tirageId = $request->input('tirage');
+        $date = $request->input('date');
+        $unchiffre = $request->input('unchiffre');
+        $premierchiffre = $request->input('premierchiffre');
+        $secondchiffre = $request->input('secondchiffre');
+        $troisiemechiffre = $request->input('troisiemechiffre');
+
+        $erreurs = $this->validerEntrees($tirageId, $unchiffre, $premierchiffre, $secondchiffre, $troisiemechiffre);
 
         if ($erreurs) {
-             $messagesErreur = implode(', ', $erreurs);
-             notify()->error($messagesErreur);
-             return redirect()->back();
+            $messagesErreur = implode(', ', $erreurs);
+            notify()->error($messagesErreur);
+            return redirect()->back();
         }
 
-                //verifaction exist lo
-                $resultExist=$this->ifExisteLo($tirageId,$date);
-                if($resultExist){
-                    notify()->error('Lo sa existe deja');
-                    return redirect()->back();
-                }
+        //verifaction exist lo
+        $resultExist = $this->ifExisteLo($tirageId, $date);
+        if ($resultExist) {
+            notify()->error('Lo sa existe deja');
+            return redirect()->back();
+        }
 
 
-                //fin
-                  // Vérifier si l'heure du serveur est supérieure ou égale à $heureTirage
-                $compagnieId=session('loginId');
-                $heureTirage = tirage_record::where('tirage_id', $tirageId)->where('compagnie_id',$compagnieId)->value('hour');
-   
-     
-                $heureServeur = Carbon::now()->format('H:i:s');
-                
-                if (Carbon::parse($heureServeur)->gte(Carbon::parse($heureTirage))) {
-                    notify()->error('Le pou tiraj sa fenmen poko rive');
-                    return redirect()->back();
-                }
-                  //fin de la verification
+        //fin
+        // Vérifier si l'heure du serveur est supérieure ou égale à $heureTirage
+        $compagnieId = session('loginId');
+        $heureTirage = tirage_record::where('tirage_id', $tirageId)->where('compagnie_id', $compagnieId)->value('hour');
+
+
+        $heureServeur = Carbon::now()->format('H:i:s');
+
+        if (Carbon::parse($heureServeur)->gte(Carbon::parse($heureTirage))) {
+            notify()->error('Le pou tiraj sa fenmen poko rive');
+            return redirect()->back();
+        }
+        //fin de la verification
 
         $formattedDate = $date; //Carbon::createFromFormat('m-d-Y', $date)->format('Y-m-d');
         try {
-            $reponseadd=BoulGagnant::create([
-                'tirage_id'=>$request->input('tirage'),
+            $reponseadd = BoulGagnant::create([
+                'tirage_id' => $request->input('tirage'),
                 'compagnie_id' => session('loginId'),
                 'unchiffre' => $unchiffre,
-                'premierchiffre'=>$premierchiffre,
-                'secondchiffre'=>$secondchiffre,
-                'troisiemechiffre'=>$troisiemechiffre,
-                'etat'=>'true',
-                'created_'=>$formattedDate
+                'premierchiffre' => $premierchiffre,
+                'secondchiffre' => $secondchiffre,
+                'troisiemechiffre' => $troisiemechiffre,
+                'etat' => 'true',
+                'created_' => $formattedDate
             ]);
-    
-           if($reponseadd){
-            $class=new executeTirageController();
-            $reponse=$class->verification($tirageId,$date);
-           if($reponse=='1'){
-            notify()->success('Lo ajout avek sikese');
-            return redirect()->back();
-           }elseif($reponse=='-1'){
-            notify()->error('Pa gen fich ki jwe pou tiraj sa');
-            return redirect()->back();
-           }elseif($reponse=='0'){
-            notify()->error('Le pou tiraj fenmen poko rive');
-            return redirect()->back();
-           }
-           
-           }
+
+            if ($reponseadd) {
+                $class = new executeTirageController();
+                $reponse = $class->verification($tirageId, $date);
+                if ($reponse == '1') {
+                    notify()->success('Lo ajout avek sikese');
+                    return redirect()->back();
+                } elseif ($reponse == '-1') {
+                    notify()->error('Pa gen fich ki jwe pou tiraj sa');
+                    return redirect()->back();
+                } elseif ($reponse == '0') {
+                    notify()->error('Le pou tiraj fenmen poko rive');
+                    return redirect()->back();
+                }
+            }
         } catch (\Exception $e) {
             // Gérer l'exception si la création échoue
-            notify()->error('erreur dajout',$e);
+            notify()->error('erreur dajout', $e);
             return redirect()->back();
-          
         }
-           
     }
 
 
-function ifExisteLo($id,$date){
-    $exist=BoulGagnant::where('compagnie_id',session('loginId'))->where('tirage_id',$id)->where('created_', $date)->get();
-    if($exist){
-        return true;
-    }else{
-        return false;
+    function ifExisteLo($id, $date)
+    {
+        $exist = BoulGagnant::where('compagnie_id', session('loginId'))->where('tirage_id', $id)->where('created_', $date)->get();
+        if ($exist->count() > 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
-  
-}
 
     function validerEntrees($tirageId, $unchiffre, $premierchiffre, $secondchiffre, $troisiemechiffre)
-{
-    $validator = Validator::make(
-        [
-            'tirage' => $tirageId,
-            'unchiffre' => $unchiffre,
-            'premierchiffre' => $premierchiffre,
-            'secondchiffre' => $secondchiffre,
-            'troisiemechiffre' => $troisiemechiffre,
-        ],
-        [
-            'tirage' => 'required|integer',
-            'unchiffre' => 'required|integer|digits:1',
-            'premierchiffre' => 'required|integer|digits:2',
-            'secondchiffre' => 'required|integer|digits:2',
-            'troisiemechiffre' => 'required|integer|digits:2',
-        ],
-        [
-            'unchiffre.digits' => 'Yon chif dwe yon sel chif.',
-            'premierchiffre.digits' => 'Pwemye lot dwe 2chif',
-            'secondchiffre.digits' => 'Dezyem lo dwe 2 chif.',
-            'troisiemechiffre.digits' => 'twazyem lot dwe 2 chif',
-        ]
-    );
+    {
+        $validator = Validator::make(
+            [
+                'tirage' => $tirageId,
+                'unchiffre' => $unchiffre,
+                'premierchiffre' => $premierchiffre,
+                'secondchiffre' => $secondchiffre,
+                'troisiemechiffre' => $troisiemechiffre,
+            ],
+            [
+                'tirage' => 'required|integer',
+                'unchiffre' => 'required|integer|digits:1',
+                'premierchiffre' => 'required|integer|digits:2',
+                'secondchiffre' => 'required|integer|digits:2',
+                'troisiemechiffre' => 'required|integer|digits:2',
+            ],
+            [
+                'unchiffre.digits' => 'Yon chif dwe yon sel chif.',
+                'premierchiffre.digits' => 'Pwemye lot dwe 2chif',
+                'secondchiffre.digits' => 'Dezyem lo dwe 2 chif.',
+                'troisiemechiffre.digits' => 'twazyem lot dwe 2 chif',
+            ]
+        );
 
-    if ($validator->fails()) {
-        return $validator->errors()->all();
-    }
+        if ($validator->fails()) {
+            return $validator->errors()->all();
+        }
 
-    return null; // Aucune erreur de validation
-}
-
-
-public function modifierlo(Request $request){
-    $tirageId=$request->input('tirage');
-    $date=$request->input('date');
-    $unchiffre=$request->input('unchiffre');
-    $premierchiffre=$request->input('premierchiffre');
-    $secondchiffre=$request->input('secondchiffre');
-    $troisiemechiffre=$request->input('troisiemechiffre');
-
-    $erreurs =$this->validerEntrees($tirageId, $unchiffre, $premierchiffre, $secondchiffre, $troisiemechiffre);
-
-    if ($erreurs) {
-         $messagesErreur = implode(', ', $erreurs);
-         notify()->error($messagesErreur);
-         return redirect()->back();
+        return null; // Aucune erreur de validation
     }
 
 
-    $Boulgnant=BoulGagnant::where('compagnie_id',session('loginId'))->where('created_',$date);
-    if($Boulgnant){
-    try {
-        $reponseadd=$Boulgnant->update([
-            'unchiffre' => $unchiffre,
-            'premierchiffre'=>$premierchiffre,
-            'secondchiffre'=>$secondchiffre,
-            'troisiemechiffre'=>$troisiemechiffre,
-            'etat'=>'true',
-        ]);
+    public function modifierlo(Request $request)
+    {
+        $tirageId = $request->input('tirage');
+        $date = $request->input('date');
+        $unchiffre = $request->input('unchiffre');
+        $premierchiffre = $request->input('premierchiffre');
+        $secondchiffre = $request->input('secondchiffre');
+        $troisiemechiffre = $request->input('troisiemechiffre');
 
-       
-       if($reponseadd){
-        $class=new executeTirageController();
-        $reponse=$class->verification($tirageId,$date);
-       if($reponse=='1'){
-        notify()->success('Lo Modifier et triyaj sikese');
-        return redirect()->back();
-       }elseif($reponse=='-1'){
-        notify()->error('Pa gen fich ki jwe pou tiraj sa');
-        return redirect()->back();
-       }elseif($reponse=='0'){
-        notify()->error('Le pou tiraj fenmen poko rive');
-        return redirect()->back();
-       }
-       
-       }
-       notify()->success('Lo Modifier e triyaj sikese');
-        return redirect()->route('listlo');
-    } catch (\Exception $e) {
-        // Gérer l'exception si la création échoue
-        notify()->error('erreur dajout',$e);
-        return redirect()->back();
-      
+        $erreurs = $this->validerEntrees($tirageId, $unchiffre, $premierchiffre, $secondchiffre, $troisiemechiffre);
+
+        if ($erreurs) {
+            $messagesErreur = implode(', ', $erreurs);
+            notify()->error($messagesErreur);
+            return redirect()->back();
+        }
+
+
+        $Boulgnant = BoulGagnant::where('compagnie_id', session('loginId'))->where('created_', $date);
+        if ($Boulgnant) {
+            try {
+                $reponseadd = $Boulgnant->update([
+                    'unchiffre' => $unchiffre,
+                    'premierchiffre' => $premierchiffre,
+                    'secondchiffre' => $secondchiffre,
+                    'troisiemechiffre' => $troisiemechiffre,
+                    'etat' => 'true',
+                ]);
+
+
+                if ($reponseadd) {
+                    $class = new executeTirageController();
+                    $reponse = $class->verification($tirageId, $date);
+                    if ($reponse == '1') {
+                        notify()->success('Lo Modifier et triyaj sikese');
+                        return redirect()->back();
+                    } elseif ($reponse == '-1') {
+                        notify()->error('Pa gen fich ki jwe pou tiraj sa');
+                        return redirect()->back();
+                    } elseif ($reponse == '0') {
+                        notify()->error('Le pou tiraj fenmen poko rive');
+                        return redirect()->back();
+                    }
+                }
+                notify()->success('Lo Modifier e triyaj sikese');
+                return redirect()->route('listlo');
+            } catch (\Exception $e) {
+                // Gérer l'exception si la création échoue
+                notify()->error('erreur dajout', $e);
+                return redirect()->back();
+            }
+        } else {
+            notify()->error('Modification impossible');
+            return redirect()->back();
+        }
     }
-
-}else{
-    notify()->error('Modification impossible');
-    return redirect()->back();
-}
-
-}
-
-
 }
--- app/Http/Controllers/api/ticketController.php
+++ app/Http/Controllers/api/ticketController.php
@@ -10,6 +10,7 @@ use App\Http\Controllers\api\verificationController as verify;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
 use App\Models\tirage_record;
+
 class ticketController extends Controller
 {
 
@@ -17,67 +18,65 @@ class ticketController extends Controller
 
 
     public function creer_ticket(Request $request)
-    {  
-         // prix total
-        $amount_tot=0;
+    {
+        // prix total
+        $amount_tot = 0;
         //tirage
-       
-        $ticketId ="";
-       // $array = $request->all();
-       // $allBolete = $array['bolete'];
-        
 
-     //trouver compagnie
+        $ticketId = "";
+        // $array = $request->all();
+        // $allBolete = $array['bolete'];
+
+
+        //trouver compagnie
         $comp = company::where([
             ['id', '=', auth()->user()->compagnie_id],
             ['is_delete', '=', 0],
         ])->first();
-        if(!$comp){
+        if (!$comp) {
             return response()->json([
-                'status' =>'false',
-                'message' =>'compagnie non trouve',
+                'status' => 'false',
+                'message' => 'compagnie non trouve',
                 'code' => 404,
-            ],404,);
-
+            ], 404,);
         }
         //verifier si compagnie bloquer
-        if($comp->is_block =='1'){
+        if ($comp->is_block == '1') {
             return response()->json([
-                'status' =>'false',
-                'message' =>'compagnie bloque',
+                'status' => 'false',
+                'message' => 'compagnie bloque',
                 'code' => 404,
-            ],404,);
+            ], 404,);
         }
         //trouver vendeur
         $vendeur = User::where([
             ['id', '=', auth()->user()->id],
             ['is_delete', '=', 0],
         ])->first();
-        if(!$vendeur){
+        if (!$vendeur) {
             return response()->json([
-                'status' =>'false',
-                'message' =>'vendeur non trouve',
+                'status' => 'false',
+                'message' => 'vendeur non trouve',
                 'code' => '404',
-            ],404,);
-
+            ], 404,);
         }
         //verifier si vendeur bloquer
-        if($vendeur->is_block =='1'){
+        if ($vendeur->is_block == '1') {
             return response()->json([
-                'status' =>'false',
-                'message' =>'vendeur bloque',
+                'status' => 'false',
+                'message' => 'vendeur bloque',
                 'code' => '404',
-            ],404,);
+            ], 404,);
         }
         $resp = verify::verifierBoulesNonAutorisees($request);
         if ($resp != '1') {
             return $resp;
         }
-        $i =0;
+        $i = 0;
         //$ticketId[];
         foreach ($request->input('tirages') as $name) {
-           
-  
+
+
             $tirage_record = tirage_record::where([
                 ['compagnie_id', '=', auth()->user()->compagnie_id],
                 ['is_active', '=', '1'],
@@ -87,70 +86,200 @@ class ticketController extends Controller
                 '>',
                 Carbon::now()->format('H:i:s'),
             )->first();
-            $tirage[] = $tirage_record->name.', '.$tirage_record->hour_tirer;
-            if(!$tirage_record){
+            $tirage[] = $tirage_record->name . ', ' . $tirage_record->hour_tirer;
+            if (!$tirage_record) {
                 return response()->json([
-                    'status' =>'false',
-                    'message' =>'tirage ferme',
+                    'status' => 'false',
+                    'message' => 'tirage ferme',
                     'code' => '404',
-                ],404,);
-
+                ], 404,);
             }
             $montant = verify::calculer_montant($request);
             $amount_tot = $amount_tot + $montant;
-            
-            if($i=='0'){
+
+            if ($i == '0') {
                 $created_at = Carbon::now();
                 $ticketId = time() . '-' . rand(1000, 9999);
                 $query = DB::table('ticket_code')->insertGetId([
                     'code' => $ticketId,
                     'user_id' => auth()->user()->id,
                     'compagnie_id' => auth()->user()->compagnie_id,
-                    'created_at' =>  $created_at,     
+                    'created_at' =>  $created_at,
                 ]);
-                $boule [] = ['bolete' => $request->input('bolete')];
-                $boule [] = ['maryaj' => $request->input('maryaj')];
-                $boule [] = ['loto3' => $request->input('loto3')];
-                $boule [] = ['loto4' => $request->input('loto4')];
-                $boule [] = ['loto5' => $request->input('loto5')];
-                $boule [] = ['mariage-gratis' => []];
-
+                $boule[] = ['bolete' => $request->input('bolete')];
+                $boule[] = ['maryaj' => $request->input('maryaj')];
+                $boule[] = ['loto3' => $request->input('loto3')];
+                $boule[] = ['loto4' => $request->input('loto4')];
+                $boule[] = ['loto5' => $request->input('loto5')];
+                $boule[] = ['mariage-gratis' => []];
             }
             $query = DB::table('ticket_vendu')->insertGetId([
                 'ticket_code_id' => $ticketId,
                 'tirage_record_id' => $tirage_record->id,
-                'boule' =>json_encode($boule),
-                'amount' =>  $montant, 
-                'created_at' =>  $created_at,     
+                'boule' => json_encode($boule),
+                'amount' =>  $montant,
+                'created_at' =>  $created_at,
             ]);
-           
-            $i++;
 
-           
-           
-       
+            $i++;
         }
         return response()->json([
-            'status' =>'true',
-            'message' =>'success',
+            'status' => 'true',
+            'message' => 'success',
             'code' => '200',
-            'head'=>[
-                'compagnie'=>$comp->name,
-                'bank'=> $vendeur->bank_name,
-                '#ticket'=>$ticketId,
-                'date'=>$created_at->format('d-m-y, H:i:s'),
-                'tirage'=> $tirage
+            'head' => [
+                'compagnie' => $comp->name,
+                'bank' => $vendeur->bank_name,
+                '#ticket' => $ticketId,
+                'date' => $created_at->format('d-m-y, H:i:s'),
+                'tirage' => $tirage
             ],
-            'body'=> $boule,
+            'body' => $boule,
 
-            'foot'=>[
-                'motant'=> $amount_tot,
-                'info'=> $comp->info,
+            'foot' => [
+                'motant' => $amount_tot,
+                'info' => $comp->info,
             ]
-        ],200,);
-        
-      
-      
+        ], 200,);
+    }
+    public function list_ticket(Request $request)
+    {
+        $validator = $request->validate([
+            "date_debut" => "required",
+            "date_fin" => "required",
+            "tirage" => "required",
+        ]);
+         try{
+            if (!empty($request->input('tirage') == 'tout')) {
+                $vente = DB::table('ticket_code')->where([
+                    ['compagnie_id', '=', auth()->user()->compagnie_id],
+                    ['user_id', '=', auth()->user()->id],
+                ])->whereDate('ticket_code.created_at', '>=', $request->date_debut)
+                    ->whereDate('ticket_code.created_at', '<=', $request->date_fin)
+                    ->join('ticket_vendu', 'ticket_vendu.ticket_code_id', '=', 'ticket_code.code')
+                    ->join('tirage_record', 'tirage_record.id', '=', 'ticket_vendu.tirage_record_id');
+            }
 
+         } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                "message" => $th->getMessage(),
+
+            ], 500,);
+        }
+        
+
+        $comp = company::where([
+            ['id', '=', auth()->user()->compagnie_id],
+            ['is_delete', '=', 0],
+        ])->first();
+        if (!$comp) {
+            return response()->json([
+                'status' => 'false',
+                'message' => 'compagnie non trouve',
+                'code' => 404,
+            ], 404,);
+        }
+        //verifier si compagnie bloquer
+        if ($comp->is_block == '1') {
+            return response()->json([
+                'status' => 'false',
+                'message' => 'compagnie bloque',
+                'code' => 404,
+            ], 404,);
+        }
+        //trouver vendeur
+        $vendeur = User::where([
+            ['id', '=', auth()->user()->id],
+            ['is_delete', '=', 0],
+        ])->first();
+        if (!$vendeur) {
+            return response()->json([
+                'status' => 'false',
+                'message' => 'vendeur non trouve',
+                'code' => '404',
+            ], 404,);
+        }
+        //verifier si vendeur bloquer
+        if ($vendeur->is_block == '1') {
+            return response()->json([
+                'status' => 'false',
+                'message' => 'vendeur bloque',
+                'code' => '404',
+            ], 404,);
+        }
+        $resp = verify::verifierBoulesNonAutorisees($request);
+        if ($resp != '1') {
+            return $resp;
+        }
+        $i = 0;
+        //$ticketId[];
+        foreach ($request->input('tirages') as $name) {
+
+
+            $tirage_record = tirage_record::where([
+                ['compagnie_id', '=', auth()->user()->compagnie_id],
+                ['is_active', '=', '1'],
+                ['name', '=', $name['name']]
+            ])->whereTime(
+                'hour',
+                '>',
+                Carbon::now()->format('H:i:s'),
+            )->first();
+            $tirage[] = $tirage_record->name . ', ' . $tirage_record->hour_tirer;
+            if (!$tirage_record) {
+                return response()->json([
+                    'status' => 'false',
+                    'message' => 'tirage ferme',
+                    'code' => '404',
+                ], 404,);
+            }
+            $montant = verify::calculer_montant($request);
+            $amount_tot = $amount_tot + $montant;
+
+            if ($i == '0') {
+                $created_at = Carbon::now();
+                $ticketId = time() . '-' . rand(1000, 9999);
+                $query = DB::table('ticket_code')->insertGetId([
+                    'code' => $ticketId,
+                    'user_id' => auth()->user()->id,
+                    'compagnie_id' => auth()->user()->compagnie_id,
+                    'created_at' =>  $created_at,
+                ]);
+                $boule[] = ['bolete' => $request->input('bolete')];
+                $boule[] = ['maryaj' => $request->input('maryaj')];
+                $boule[] = ['loto3' => $request->input('loto3')];
+                $boule[] = ['loto4' => $request->input('loto4')];
+                $boule[] = ['loto5' => $request->input('loto5')];
+                $boule[] = ['mariage-gratis' => []];
+            }
+            $query = DB::table('ticket_vendu')->insertGetId([
+                'ticket_code_id' => $ticketId,
+                'tirage_record_id' => $tirage_record->id,
+                'boule' => json_encode($boule),
+                'amount' =>  $montant,
+                'created_at' =>  $created_at,
+            ]);
+
+            $i++;
+        }
+        return response()->json([
+            'status' => 'true',
+            'message' => 'success',
+            'code' => '200',
+            'head' => [
+                'compagnie' => $comp->name,
+                'bank' => $vendeur->bank_name,
+                '#ticket' => $ticketId,
+                'date' => $created_at->format('d-m-y, H:i:s'),
+                'tirage' => $tirage
+            ],
+            'body' => $boule,
+
+            'foot' => [
+                'motant' => $amount_tot,
+                'info' => $comp->info,
+            ]
+        ], 200,);
     }
 }
